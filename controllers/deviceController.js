455ced1010def8f7c01094cebf9c9c8f2a15af33
XsportsShoppingMall <chalgaro46@gmail.com>
2024-03-04 02:32:58 +0900

    devicemodel

---
+++ controllers/deviceController.js
@@ -0,0 +1,38 @@
+const Device = require('../models/deviceModel'); 
+
+const searchDevices = async (req, res) => {
+    const { modelNameKeyword, serialNumKeyword, manufacturerKeyword, conditionKeyword, storageLocationKeyword, page } = req.query;
+
+    Device.searchDevices(modelNameKeyword, serialNumKeyword, manufacturerKeyword, conditionKeyword, storageLocationKeyword, page, (error, results) => {
+        if (error) {
+            console.error('Error in searching device details:', error);
+            res.status(500).json({ error: 'Internal server error' });
+        } else {
+            res.json(results);
+        }
+    });
+};
+
+const getDeviceById = async (req, res) => {
+    const { deviceId } = req.params;
+    try {
+      const results = await new Promise((resolve, reject) => {
+        Device.getDeviceById(deviceId, (error, data) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(data);
+          }
+        });
+      });
+      res.status(200).json(results);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  };
+  
+
+module.exports = {
+    searchDevices,
+    getDeviceById,
+};
---
+++ routers/deviceRouters.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const deviceController = require('../controllers/deviceController');
+
+// 기기 세부 정보 검색 및 페이지네이션을 위한 라우트
+router.get('/search', deviceController.searchDevices);
+router.get('/byId/:deviceId', deviceController.getDeviceById);
+module.exports = router;
