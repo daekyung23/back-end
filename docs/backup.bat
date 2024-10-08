063e03833b3ea3081f92b20511a701e3b02bc79e
dongkeonkim-dev <chalgaro46@gmail.com>
2024-10-08 22:11:04 +0900

    backup update

--- docs/backup.bat
+++ docs/backup.bat
@@ -5,9 +5,18 @@ set mysqlUser=root
 set mysqlPassword=admin
 set databaseName=mydb
 
-REM MySQL installation path (mysqldump.exe full path)
-set mysqlDumpPath="C:\Program Files\MySQL\MySQL Server 8.0\bin\mysqldump.exe"
-
+REM MySQL installation path check for both C: and E:
+if exist "C:\Program Files\MySQL\MySQL Server 8.0\bin\mysqldump.exe" (
+    set mysqlDumpPath="C:\Program Files\MySQL\MySQL Server 8.0\bin\mysqldump.exe"
+    echo MySQL found on C drive.
+) else if exist "E:\Program Files\MySQL\MySQL Server 8.0\bin\mysqldump.exe" (
+    set mysqlDumpPath="E:\Program Files\MySQL\MySQL Server 8.0\bin\mysqldump.exe"
+    echo MySQL found on E drive.
+) else (
+    echo MySQL installation not found on C: or E: drive.
+    pause
+    exit /b
+)
 REM Set backup file path and current date/time (in non-Korean format)
 for /f "skip=1 tokens=1" %%a in ('wmic os get localdatetime') do set datetime=%%a
 set date=%datetime:~0,8%
