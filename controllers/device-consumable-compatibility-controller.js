84ddee182ec9a7cd59ae2dad2630511022c5de3f
dongkeonkim-dev <chalgaro46@gmail.com>
2024-10-18 02:45:45 +0900

    11

--- controllers/device-consumable-compatibility-controller.js
+++ controllers/device-consumable-compatibility-controller.js
@@ -1,34 +1,34 @@
-const deviceConsumableCompatibilityRepository = require('../repositories/device-consumable-compatibility-repository');
-const { isValid, toValidate, validateFields } = require('../utils/validation'); 
+// const deviceConsumableCompatibilityRepository = require('../repositories/device-consumable-compatibility-repository');
+// const { isValid, toValidate, validateFields } = require('../utils/validation'); 
 
-const createAllDeviceConsumableCompatibility = async (req, res) => {
-  const { consumable_model_id, device_model_id_array } = req.body;
+// const createAllDeviceConsumableCompatibility = async (req, res) => {
+//   const { consumable_model_id, device_model_id_array } = req.body;
 
-  try {
-    await deviceConsumableCompatibilityRepository.createAllDeviceConsumableCompatibility(consumable_model_id, device_model_id_array);
-    res.json({ message: 'Device created' });
-  } catch (error) {
-    console.error('Error in createDevice controller:', error);
-    res.status(500).json({ error: 'Failed to create device' });
-  }
-};
+//   try {
+//     await deviceConsumableCompatibilityRepository.createAllDeviceConsumableCompatibility(consumable_model_id, device_model_id_array);
+//     res.json({ message: 'Device created' });
+//   } catch (error) {
+//     console.error('Error in createDevice controller:', error);
+//     res.status(500).json({ error: 'Failed to create device' });
+//   }
+// };
 
-const deleteAllDeviceConsumableCompatibilityByConsumableModelId = async (req, res) => {
-  const { consumable_model_id } = req.query;
-  if (!isValid(consumable_model_id)) {
-    return res.status(400).json({ error: 'Missing consumable_model_id' });
-  }
+// const deleteAllDeviceConsumableCompatibilityByConsumableModelId = async (req, res) => {
+//   const { consumable_model_id } = req.query;
+//   if (!isValid(consumable_model_id)) {
+//     return res.status(400).json({ error: 'Missing consumable_model_id' });
+//   }
 
-  try {
-    await deviceConsumableCompatibilityRepository.deleteAllDeviceConsumableCompatibilityByConsumableModelId(consumable_model_id);
-    res.json({ message: 'Device deleted' });
-  } catch (error) {
-    console.error('Error in deleteDevice controller:', error);
-    res.status(500).json({ error: 'Failed to delete device' });
-  }
-}
+//   try {
+//     await deviceConsumableCompatibilityRepository.deleteAllDeviceConsumableCompatibilityByConsumableModelId(consumable_model_id);
+//     res.json({ message: 'Device deleted' });
+//   } catch (error) {
+//     console.error('Error in deleteDevice controller:', error);
+//     res.status(500).json({ error: 'Failed to delete device' });
+//   }
+// }
 
-module.exports = {
-  createAllDeviceConsumableCompatibility,
-  deleteAllDeviceConsumableCompatibilityByConsumableModelId,
-};
+// module.exports = {
+//   createAllDeviceConsumableCompatibility,
+//   deleteAllDeviceConsumableCompatibilityByConsumableModelId,
+// };
