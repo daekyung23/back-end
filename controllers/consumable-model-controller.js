e263e62dfff09fab9aedd49d7c88f70522752a3c
dongkeonkim-dev <chalgaro46@gmail.com>
2024-10-18 03:03:47 +0900

    123

--- controllers/consumable-model-controller.js
+++ controllers/consumable-model-controller.js
@@ -45,9 +45,9 @@ const createConsumableModel = async (req, res) => {
   const requiredFields = { manufacturer, consumable_name, consumable_type };
   const validationError = validateFields(requiredFields, res);
   if (validationError) return validationError;
-  if(await consumableModelRepository.checkDuplicateConsumableModel(consumable_name)) {
-    return res.status(400).json({ message: 'Duplicate consumable model' });
-  }
+  // if(await consumableModelRepository.checkDuplicateConsumableModel(consumable_name)) {
+  //   return res.status(400).json({ message: 'Duplicate consumable model' });
+  // }
   const consumableModel = { ...requiredFields };
 
   try {
