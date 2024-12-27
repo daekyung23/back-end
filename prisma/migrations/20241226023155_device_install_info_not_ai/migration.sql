3cabc6d76e0b7f15e0eff30c61c6662d03525a46
KimAman <chalgaro46@gmail.com>
2024-12-27 16:45:56 +0900

    backup file update

--- prisma/migrations/20241226023155_device_install_info_not_ai/migration.sql
+++ prisma/migrations/20241226023155_device_install_info_not_ai/migration.sql
@@ -1,15 +1,6 @@
--- drop constraint
-ALTER TABLE `device_install_info`
-DROP FOREIGN KEY `fk_device_install_info_device1`;
+-- AlterTable
+SET FOREIGN_KEY_CHECKS=0;
 
--- modify column
-ALTER TABLE `device_install_info`
-MODIFY `device_id` INTEGER NOT NULL;
+ALTER TABLE `device` MODIFY `device_id` INTEGER NOT NULL AUTO_INCREMENT;
 
--- re-add constraint
-ALTER TABLE `device_install_info`
-ADD CONSTRAINT `fk_device_install_info_device1`
-  FOREIGN KEY (`device_id`)
-  REFERENCES `device` (`device_id`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
+SET FOREIGN_KEY_CHECKS=1;
